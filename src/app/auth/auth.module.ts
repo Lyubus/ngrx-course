fad0d8a942373535d7b5543079450ae513f80377
Oleg Lyubuskiy <lyubuskiyy@gmail.com>
2022-03-17 19:38:39 +0200

    LES-11 Add auth reducer

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -9,6 +9,7 @@ import {MatButtonModule} from '@angular/material/button';
 import { StoreModule } from '@ngrx/store';
 import {AuthService} from './auth.service';
 import * as fromAuth from './reducers';
+import { authReducer } from './reducers';
 
 @NgModule({
     imports: [
@@ -18,7 +19,7 @@ import * as fromAuth from './reducers';
         MatInputModule,
         MatButtonModule,
         RouterModule.forChild([{path: '', component: LoginComponent}]),
-        StoreModule.forFeature(fromAuth.authFeatureKey, fromAuth.reducers),
+        StoreModule.forFeature(fromAuth.authFeatureKey, authReducer),
     ],
     declarations: [LoginComponent],
     exports: [LoginComponent]
