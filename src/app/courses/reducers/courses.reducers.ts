db73b2ca4b65196d9b2f6bb059f98d69fe9a85a8
Oleg Lyubuskiy <lyubuskiyy@gmail.com>
2022-03-27 16:37:14 +0300

    LES-33 Call fetch data only when needed

--- src/app/courses/reducers/courses.reducers.ts
+++ src/app/courses/reducers/courses.reducers.ts
@@ -3,17 +3,21 @@ import { createReducer, on } from '@ngrx/store';
 import { CourseActions } from '../action.types';
 import { compareCourses, Course } from '../model/course';
 
-export interface CoursesState extends EntityState<Course> { }
+export interface CoursesState extends EntityState<Course> {
+	allCoursesLoaded: boolean;
+}
 
 export const adapter = createEntityAdapter<Course>({
 	sortComparer: compareCourses
 });
 
-export const initialCoursesState = adapter.getInitialState();
+export const initialCoursesState = adapter.getInitialState({
+	allCoursesLoaded: false
+});
 
 export const coursesReducer = createReducer(
 	initialCoursesState,
-	on(CourseActions.allCoursesLoaded, (state, action) => adapter.addAll(action.courses, state))
+	on(CourseActions.allCoursesLoaded, (state, action) => adapter.addAll(action.courses, {...state, allCoursesLoaded: true}))
 );
 
 export const { selectAll } = adapter.getSelectors();
